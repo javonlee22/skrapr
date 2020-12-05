7bb8d49fc8a6e0ea856e23ba524c8c67be32c3d8
javonlee22 <javonlee411@gmail.com>
2020-12-05 05:43:04 -0500

    Initial commit

---
+++ src/main/java/com/technoformers/skrapr/SkraprApplication.java
@@ -0,0 +1,13 @@
+package com.technoformers.skrapr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkraprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkraprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technoformers/skrapr/SkraprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technoformers.skrapr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkraprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
